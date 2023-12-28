afce18992324b3643f7c8d1626b949869d648af3
gramaphenia <dicoratojuanpablo@gmail.com>
2023-12-28 18:47:05 -0300

    mods-galeria

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -8,6 +8,9 @@ import BuconeroStandart from "@/public/miniaturas/buconero-standart.png";
 import EiraStandart from "@/public/miniaturas/eira-standart.png";
 import VellutoStandart from "@/public/miniaturas/velluto-standart.png";
 import Es35Nativa from "@/public/miniaturas/es-35-nativa.png";
+import BuconeroCustomNativa from "@/public/miniaturas/buconero-custom-nativa.png";
+import ClasicaDos from "@/public/miniaturas/clasica-dos.png";
+import BuconeroBoutique from "@/public/miniaturas/buconero-boutique.png";
 
 export const metadata = {
   title: "Galeria de imagenes",
@@ -20,7 +23,7 @@ export default function Galeria() {
         <div className="relative rounded-xl overflow-auto px-0 my-4">
           <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
             <Image
-              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-[--border-light]"
               src={Imperatrice}
             ></Image>
             <div className="flex flex-col py-5 pl-24">
@@ -93,7 +96,7 @@ export default function Galeria() {
             ></Image>
             <div className="flex flex-col py-5 pl-24">
               <strong className="text-sm font-medium dark:text-slate-200">
-              Es35 Nativa
+                Es35 Nativa
               </strong>
               <span className="text-sm font-medium dark:text-slate-400">
                 Someone
@@ -101,15 +104,50 @@ export default function Galeria() {
             </div>
           </div>
         </div>
+
         <div className="relative rounded-xl overflow-auto px-0 my-4 ">
           <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
             <Image
               className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
-              src={Es35Nativa}
+              src={BuconeroCustomNativa}
             ></Image>
             <div className="flex flex-col py-5 pl-24">
               <strong className="text-sm font-medium dark:text-slate-200">
-              Es35 Nativa
+                Buconero Custom Nativa
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={ClasicaDos}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                Clasica II
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={BuconeroBoutique}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                Buconero Boutique
               </strong>
               <span className="text-sm font-medium dark:text-slate-400">
                 Someone

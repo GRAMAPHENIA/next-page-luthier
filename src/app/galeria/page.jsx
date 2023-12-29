75dfcd8f4da26858794efadddc2c84619f963b77
gramaphenia <dicoratojuanpablo@gmail.com>
2023-12-29 09:11:58 -0300

    galeria

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -11,6 +11,11 @@ import Es35Nativa from "@/public/miniaturas/es-35-nativa.png";
 import BuconeroCustomNativa from "@/public/miniaturas/buconero-custom-nativa.png";
 import ClasicaDos from "@/public/miniaturas/clasica-dos.png";
 import BuconeroBoutique from "@/public/miniaturas/buconero-boutique.png";
+import VellutoARN from "@/public/miniaturas/velluto-arn.png";
+import SmallyPequestrat from "@/public/miniaturas/smally-pequestrat.png";
+import LPJuniorCustom from "@/public/miniaturas/lp-junior-custom.png";
+import ClasicaDosDos from "@/public/miniaturas/clasica-dos-dos.png";
+import PBassCustom from "@/public/miniaturas/p-bass-custom.png";
 
 export const metadata = {
   title: "Galeria de imagenes",
@@ -19,11 +24,11 @@ export const metadata = {
 export default function Galeria() {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 relative rounded-2xl overflow-hidden bg-[var(--primary)] mt-10 py-4 px-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 relative rounded-2xl overflow-hidden bg-[var(--primary)] my-10 py-4 px-6">
         <div className="relative rounded-xl overflow-auto px-0 my-4">
           <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5  rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
             <Image
-              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-[--border-light]"
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
               src={Imperatrice}
             ></Image>
             <div className="flex flex-col py-5 pl-24">
@@ -148,6 +153,91 @@ export default function Galeria() {
             <div className="flex flex-col py-5 pl-24">
               <strong className="text-sm font-medium dark:text-slate-200">
                 Buconero Boutique
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={VellutoARN}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                Velluto ARN
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={SmallyPequestrat}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                Smally Pequestrat
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={LPJuniorCustom}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                LP junior custom
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={ClasicaDosDos}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                Clasica II
+              </strong>
+              <span className="text-sm font-medium dark:text-slate-400">
+                Someone
+              </span>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative rounded-xl overflow-auto px-0 my-4 ">
+          <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5 rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+            <Image
+              className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
+              src={PBassCustom}
+            ></Image>
+            <div className="flex flex-col py-5 pl-24">
+              <strong className="text-sm font-medium dark:text-slate-200">
+                PBass custom
               </strong>
               <span className="text-sm font-medium dark:text-slate-400">
                 Someone

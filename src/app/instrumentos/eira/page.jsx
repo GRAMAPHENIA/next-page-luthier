2992c456e1e655dc965cdaf4e3c2f0b6fd48686f
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-10 18:32:58 -0300

    plus-mods

--- src/app/instrumentos/eira/page.jsx
+++ src/app/instrumentos/eira/page.jsx
@@ -1,30 +1,128 @@
 import Image from "next/image";
-import CroquisBuconero from "/public/croquis/croquis-buconero-numerado.png";
+import CroquiseiraVert from "@/public/croquis/guitarras-numeracion/eira-vert.svg";
+import CroquiseiraHorz from "@/public/croquis/guitarras-numeracion/eira-horz.svg";
+import eiraCuerpo from "@/public/guitarras/velluto.png";
+import Gotoh from "@/public/svgs/marcas/gotoh.svg";
+import Hosco from "@/public/svgs/marcas/hosco.svg";
+import Diliberto from "@/public/svgs/marcas/diliberto.svg";
+import Wilkinson from "@/public/svgs/marcas/wilkinson.svg";
 
 export const metadata = {
-  title: "Modelo Eira",
+  title: "Modelo eira",
 };
 
-export default function Eira() {
+export default function Buconero() {
+  const lista = [
+    {
+      descripcion: "Acabado poliuretano mate y brillante.",
+    },
+    {
+      descripcion: "Cuerpo de caroba en una pieza.",
+    },
+    {
+      descripcion: "24 trastes hosco 2.4 M.",
+    },
+    {
+      descripcion: "Clavijas Gotoh.",
+    },
+    {
+      descripcion: "Diapasón de ébano macasar, radio 20 .",
+    },
+    {
+      descripcion:
+        "Incrustación en traste 12 de ébano Gabón y resina fotoluminiscente.",
+    },
+    {
+      descripcion: "Mango laminado de jequetiva/ guatambú/viraro, perfil fino.",
+    },
+    {
+      descripcion: "Micrófonos Diliverto C9 special.",
+    },
+    {
+      descripcion: "Perillas en madera.",
+    },
+    {
+      descripcion: "Puente tremolo Wilkinson wvp.",
+    },
+    {
+      descripcion: "Tensor doble acción termosellado.",
+    },
+    {
+      descripcion: "Electrónica gotoh/swichcraft.",
+    },
+  ];
+
   return (
     <>
-      <section className="grid my-2 grid-cols-4 row-span-4 lg:grid lg:grid-cols-6 gap-2 ">
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-4 p-10 lg:p-10">
-          1
+      <section className="grid my-2 grid-cols-4 row-span-4 lg:grid lg:grid-cols-8 gap-2 ">
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-8 p-10 lg:p-10">
           <Image
-            src={CroquisBuconero}
-            alt="croquis buconero numerado"
-            width={1000}
+            src={CroquiseiraVert}
+            alt="croquis eira numerado"
+            width="auto"
+            height="auto"
+            className="mx-auto h-[600px] lg:hidden"
+          />
+          <Image
+            src={CroquiseiraHorz}
+            alt="croquis eira numerado"
+            width="auto"
+            height="auto"
+            className="hidden lg:block"
+          />
+        </figure>
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-8 lg:col-span-5 p-10 lg:p-10">
+          <ol className="space-y-2 text-sm lg:text-base">
+            {lista.map((item, index) => (
+              <li key={index}>
+                <p className="text-[var(--text-light)]">
+                  <span className="text-amber-100 mr-4 ">{index + 1}.</span>
+                  {item.descripcion}
+                </p>
+              </li>
+            ))}
+          </ol>
+        </figure>
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-8 lg:col-span-3 p-10 lg:p-10 ">
+          <Image
+            src={eiraCuerpo}
+            alt="croquis eira numerado"
+            width="auto"
+            height="auto"
+            className="mx-auto h-[400px] w-auto"
+          />
+        </figure>
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
+          <Image
+            src={Diliberto}
+            alt="croquis eira numerado"
+            width="auto"
             height="auto"
           />
         </figure>
-
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10 lg:p-10">
-          2
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
+          <Image
+            src={Gotoh}
+            alt="croquis eira numerado"
+            width="auto"
+            height="auto"
+          />
         </figure>
-
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10 lg:p-10">
-          3
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
+          <Image
+            src={Hosco}
+            alt="croquis eira numerado"
+            width="auto"
+            height="auto"
+          />
+        </figure>
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
+          <Image
+            src={Wilkinson}
+            alt="croquis eira numerado"
+            width="auto"
+            height="auto"
+          />
         </figure>
       </section>
     </>
--- src/app/instrumentos/velluto/page.jsx
+++ src/app/instrumentos/velluto/page.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import CroquisBuconero from "@/public/croquis/guitarras-numeracion/buconero-horz.svg";
-import BuconeroCuerpo from "@/public/guitarras/buconero.png";
+import CroquisvellutoVert from "@/public/croquis/guitarras-numeracion/velluto-vert.svg";
+import CroquisvellutoHorz from "@/public/croquis/guitarras-numeracion/velluto-horz.svg";
+import BuconeroCuerpo from "@/public/guitarras/velluto.png";
 import Gotoh from "@/public/svgs/marcas/gotoh.svg";
 import Hosco from "@/public/svgs/marcas/hosco.svg";
 import Diliberto from "@/public/svgs/marcas/diliberto.svg";
 import Wilkinson from "@/public/svgs/marcas/wilkinson.svg";
 
 export const metadata = {
-  title: "Modelo Buconero",
+  title: "Modelo Velluto",
 };
 
-export default function Buconero() {
+export default function velluto() {
   const lista = [
     {
       descripcion: "Acabado poliuretano mate y brillante.",
@@ -56,62 +57,70 @@ export default function Buconero() {
       <section className="grid my-2 grid-cols-4 row-span-4 lg:grid lg:grid-cols-8 gap-2 ">
         <figure className="relative bg-[#14181b] rounded-2xl col-span-8 p-10 lg:p-10">
           <Image
-            src={CroquisBuconero}
-            alt="croquis buconero numerado"
-            width={1000}
+            src={CroquisvellutoVert}
+            alt="croquis velluto numerado"
+            width="auto"
             height="auto"
+            className="mx-auto h-[600px] lg:hidden"
+          />
+          <Image
+            src={CroquisvellutoHorz}
+            alt="croquis velluto numerado"
+            width="auto"
+            height="auto"
+            className="hidden lg:block"
           />
         </figure>
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-5 p-10 lg:p-10">
-          <ol className="space-y-2 ">
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-8 lg:col-span-5 p-10 lg:p-10">
+          <ol className="space-y-2 text-sm lg:text-base">
             {lista.map((item, index) => (
               <li key={index}>
                 <p className="text-[var(--text-light)]">
-                  <span className="text-amber-100 mr-4">{index + 1}.</span>
+                  <span className="text-amber-100 mr-4 ">{index + 1}.</span>
                   {item.descripcion}
                 </p>
               </li>
             ))}
           </ol>
         </figure>
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-3 p-10 lg:p-10 ">
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-8 lg:col-span-3 p-10 lg:p-10 ">
           <Image
             src={BuconeroCuerpo}
-            alt="croquis buconero numerado"
-            width={400}
+            alt="croquis velluto numerado"
+            width="auto"
             height="auto"
             className="mx-auto h-[400px] w-auto"
           />
-        </figure>{" "}
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-5">
+        </figure>
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
           <Image
             src={Diliberto}
-            alt="croquis buconero numerado"
-            width={1000}
+            alt="croquis velluto numerado"
+            width="auto"
             height="auto"
           />
         </figure>
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-5">
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
           <Image
             src={Gotoh}
-            alt="croquis buconero numerado"
-            width={1000}
+            alt="croquis velluto numerado"
+            width="auto"
             height="auto"
           />
         </figure>
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-5">
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
           <Image
             src={Hosco}
-            alt="croquis buconero numerado"
-            width={1000}
+            alt="croquis velluto numerado"
+            width="auto"
             height="auto"
           />
         </figure>
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-5">
+        <figure className="relative flex justify-center bg-[#14181b] rounded-2xl col-span-8 lg:col-span-2 p-5">
           <Image
             src={Wilkinson}
-            alt="croquis buconero numerado"
-            width={1000}
+            alt="croquis velluto numerado"
+            width="auto"
             height="auto"
           />
         </figure>

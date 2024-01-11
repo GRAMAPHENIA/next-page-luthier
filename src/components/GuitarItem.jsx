40a91fb981f6d9f2b3b7961cba27053103f40a3e
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-11 17:31:05 -0300

    galeria

--- src/components/GuitarItem.jsx
+++ src/components/GuitarItem.jsx
@@ -6,8 +6,8 @@ const GuitarItem = ({ guitar }) => {
 
   return (
     <div>
-      <div className="relative rounded-xl overflow-auto px-0 my-4">
-        <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5  rounded-xl flex items-center bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
+      <div className="relative rounded-xl overflow-auto px-0 my-4 group">
+        <div className="overflow-hidden relative max-w-sm mx-auto ring-1 ring-black/5  rounded-xl flex items-center hover:text-[var(--text-extra-light) bg-[var(--dark)] hover:bg-[var(--secondary)] cursor-pointer border border-white/10">
           <Image
             className="absolute -left-6 w-24 h-24 rounded-full shadow-lg border-4 border-white/10"
             src={foto_de_portada}
@@ -16,12 +16,17 @@ const GuitarItem = ({ guitar }) => {
             height={96}
           ></Image>
           <div className="flex flex-col py-5 pl-24">
-            <strong className="text-sm font-medium dark:text-slate-200">
+            <strong className="text-lg font-light text-[var(--text-light)] group-hover:text-[var(--text-extralight)]">
               {titulo}
-            </strong>
-            <span className="text-sm font-medium dark:text-slate-400">
+            </strong> 
+            {/* <span className="text-sm font-medium dark:text-slate-400">
               Someone
-            </span>
+            </span> */}
+
+
+            {/* hover:text-[var(--text-extra-light) */}
+
+
           </div>
         </div>
       </div>

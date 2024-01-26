37ef108fa9028358540e54b67e7c33ca80c866c7
gramaphenia <dicoratojuanpablo@gmail.com>
2024-01-26 07:21:28 -0300

    galeria-taller-innit

--- src/app/taller/page.jsx
+++ src/app/taller/page.jsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import CroquisBuconero from "/public/croquis/croquis-buconero-numerado.png";
+import tallerDos from "/public/taller/tallerdos.png";
+import tallerTres from "/public/taller/tallertres.png";
+import tallerCuatro from "/public/taller/tallerCuatro.png";
 
 export const metadata = {
   title: "El Taller",
@@ -9,22 +11,34 @@ export default function Taller() {
   return (
     <>
       <section className="grid my-2 grid-cols-4 row-span-4 lg:grid lg:grid-cols-6 gap-2 ">
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-4 p-10 lg:p-10">
-          1
-          {/* <Image
-            src={CroquisBuconero}
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-4 p-4 lg:p-4">
+          <Image
+            src={tallerDos}
             alt="croquis buconero numerado"
             width={1000}
             height="auto"
-          /> */}
+            className="rounded-xl"
+          />
         </figure>
 
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10 lg:p-10">
-          2
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-4 lg:p-4">
+          <Image
+            src={tallerTres}
+            alt="croquis buconero numerado"
+            width={1000}
+            height="auto"
+            className="rounded-xl"
+          />
         </figure>
 
-        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10 lg:p-10">
-          3
+        <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-4 lg:p-4">
+          <Image
+            src={tallerCuatro}
+            alt="croquis buconero numerado"
+            width={1000}
+            height="auto"
+            className="rounded-xl"
+          />
         </figure>
       </section>
     </>

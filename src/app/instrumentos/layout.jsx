c1edb43b8a4fb5aa0b5cb77cc35992f292a23076
gramaphenia <dicoratojuanpablo@gmail.com>
2024-02-28 08:56:15 -0300

    mods

--- src/app/instrumentos/layout.jsx
+++ src/app/instrumentos/layout.jsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 
-import Contactos from "@/components/Contactos";
+// import Contactos from "@/components/Contactos";
 import Footer from "@/components/Footer";
 
 export default function Layout({ children }) {
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
       </section>
 
       <div>{children}</div>
-      <Contactos />
+      {/* <Contactos /> */}
       <Footer />
     </>
   );

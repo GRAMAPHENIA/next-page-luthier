aad0287f61b89da866515252f0c34f57af8d40cc
gramaphenia <dicoratojuanpablo@gmail.com>
2024-02-28 13:48:39 -0300

    mod

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,8 @@ export default function Home() {
       <Instrumentos />
       <Reparaciones />
       <Galeria />
-      <Contactos />
       <Comentarios />
+      <Contactos />
       <Footer />
     </main>
   );

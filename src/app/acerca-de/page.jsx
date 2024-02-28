a1f9af8a0d6cbe9d9a382210825cc37f78d78c33
gramaphenia <dicoratojuanpablo@gmail.com>
2024-02-28 14:11:09 -0300

    fixs-mods

--- src/app/acerca-de/page.jsx
+++ src/app/acerca-de/page.jsx
@@ -11,16 +11,16 @@ export default function AcercaDe() {
   return (
     <>
       <section className="w-full grid lg:grid-cols-4 my-2 gap-2" id="sobremi">
-        <figure className="flex flex-col align-middle items-center justify-center  lg:col-start-1 lg:col-end-5 bg-[#14181b] rounded-2xl w-full p-20">
+        <figure className="flex flex-col align-middle items-center justify-center  lg:col-start-1 lg:col-end-5 bg-[#14181b] rounded-2xl w-full p-12 lg:p-20">
           <Image
             width={150}
             height={150}
             src={Isotipo}
             alt="Isotipo"
             priority={true}
-            className="px-4 pb-4"
+            className="lg:px-4 pb-4"
           ></Image>
-          <p className="text-center lg:text-2xl font-extralight text-[#7d8c9d] mb-10 mx-10 lg:mx-40 ">
+          <p className="text-center lg:text-2xl font-extralight text-[#7d8c9d] mb-10  lg:mx-40 ">
             A mediados de la década de los 90 me surge la intriga de comenzar la
             construcción de guitarras y bajos, y capacitándome de varias maneras
             logro afianzarme en conocimientos y experiencia. Es así que en el
@@ -32,7 +32,7 @@ export default function AcercaDe() {
             src={Esteban}
             alt="Logo"
             priority={true}
-            className="px-4 opacity-50"
+            className="lg:px-4 opacity-50"
           ></Image>
         </figure>
       </section>

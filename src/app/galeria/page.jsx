cbeb2991476192cb8a5d9692f1fe8fa3487a67be
gramaphenia <dicoratojuanpablo@gmail.com>
2024-02-28 08:51:11 -0300

    fixs-galeria

--- src/app/galeria/page.jsx
+++ src/app/galeria/page.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function Page() {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 relative rounded-2xl overflow-hidden bg-[var(--primary)] my-4 py-4 px-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 relative rounded-2xl overflow-hidden bg-[var(--primary)] my-2 py-4 px-6">
         {data.map((guitar) => (
           <GuitarItem key={guitar.id} guitar={guitar} />
         ))}
